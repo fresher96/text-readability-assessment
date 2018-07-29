d14c7715fbd87c13a9f675ac20d69dcf8c951c0d
Farouk <nothinginvain@hotmail.com>
2018-07-29 16:46:45 +0300

    updated gitignore

--- src/test/WekaTest.java
+++ src/test/WekaTest.java
@@ -10,22 +10,31 @@ import java.io.File;
 public class WekaTest
 {
 	public static void main(String args[]) throws Exception {
+		readWriteTest();
+	}
+	
+	private static void readWriteTest() throws Exception {
 		Instances data = ConverterUtils.DataSource.read("etc/features.csv");
 		
 		//data.setClassIndex(data.numAttributes() - 1);
+
+//		Instance x = data.firstInstance();
+//		System.out.println(x);
+//		System.out.println(data);
 		
-		Instance x = data.firstInstance();
-		System.out.println(x);
-		System.out.println(data);
+		
+		
+		
+		
+		
 		
 		
 		ConverterUtils.DataSink.write("etc/new_features.csv", data);
-		
-		
+
+
 //		CSVLoader loader = new CSVLoader();
 //		loader.setSource(new File("etc/features.csv"));
 //		Instances data = loader.getDataSet();
-		
 	
 	}
 }
